e0cb7def51c98dae08e35eb5a7fd3d3757ed3226
Ajay Dinakar Kandavalli <ajaydinakar6@gmail.com>
2017-02-23 11:44:58 -0600

    Update driver.py

--- assignments/shell/driver.py
+++ assignments/shell/driver.py
@@ -99,6 +99,52 @@ if __name__ == '__main__':
         print("give your shell command or type exit to exit")
 
         cmmd=raw_input("%")
+        
+        
+        
+        
+        """p
+        if cmmd.find('|'):
+            cmmd=cmmd.split('|')
+            var1=cmmd[0]
+            var2=cmmd[1]
+            one_cmd(var1)
+            var2=var2+" "+"md.txt"
+            one_cmd(var2)
+        elif cmmd.find('>'):
+            cmmd=cmmd.split('>')
+            var1=cmmd[0]
+            var2=cmmd[1]
+            one_cmd(var1)
+            temp=open('/root/mdr.txt','r')
+            ofile=open('out.txt','a')
+            for line in temp.readlines():
+                ofile.write(line)
+            temp.close()
+            ofile.close()
+        elif cmmd.find('>>'):
+            cmmd=cmmd.split('>>')
+            var1=cmmd[0]
+            var2=cmmd[1]
+            one_cmd(var1)
+            temp=open('/root/mdr.txt','r')
+            ofile=open('out.txt','a')
+            for line in temp.readlines():
+                ofile.write(line)
+            temp.close()
+            ofile.close()
+
+        elif cmmd.find('>'):
+            cmmd=cmmd.split('>')
+            var1=cmmd[0]
+            var2=cmmd[1]
+            one_cmd(var1)
+        else:
+		
+"""		
+		
+        
+        
         one_cmd(cmmd)
 
         if cmmd == 'exit':
